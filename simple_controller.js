71bdf552d87e28442ac311f9ddaab67e92e70bea
Tosin Yusuf <tosin.yusuf@gmail.com>
2015-10-27 15:52:48 +0000

    Initial version

---
+++ simple_controller.js
@@ -0,0 +1,22 @@
+app.controller('myCtrl', function($scope) { // define two properties in the model define by the scope variable
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.quantity = 12;
+    $scope.price = 5.99;
+    $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    };
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+    $scope.helloTo = {};
+    $scope.helloTo.title = "SimpleController";
+
+    $scope.product = { name: 'The Book of Trees', price: 20, currentdate : new Date(), image: 'Koala.jpg', likes: 0}
+	
+	$scope.plusOne =  function(index) { 
+  $scope.product.likes += 1; 
+};
+});
